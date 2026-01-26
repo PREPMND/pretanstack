75498426cd17da889aa0351316319d963ef2c4b3
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:14:30 +0000

    Jan 26, 2026, 5:44 PM

--- pretsq/src/pages/About.jsx
+++ pretsq/src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About(){
         
         <>
         <div>
-            <div>Movies</div>
+            <div>About</div>
         </div>
         </>
     )
---
+++ pretsq/src/pages/Movie.jsx
@@ -0,0 +1,10 @@
+export default function Movie(){
+    return(
+        
+        <>
+        <div>
+            <div>Movies</div>
+        </div>
+        </>
+    )
+}
