89dabd11d7421c19a56d5c961a2bb2d2cfea23e3
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:11:50 +0000

    Jan 26, 2026, 5:41 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -2,7 +2,11 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom'
 export default function NavRoutes(){
     return(
         <>
-
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navbar />} />
+            </Routes>
+        </BrowserRouter>
         </>
     )
 }
