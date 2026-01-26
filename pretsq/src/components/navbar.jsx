1bccb825bfb8a65031eb8b1b1dd6ca62f5778445
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 11:41:00 +0000

    Jan 26, 2026, 5:11 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -3,7 +3,7 @@ export default function Navbar(){
         <>
         <div>
             <div>
-                <div></div>
+                <div>Home</div>
                  
             </div>
             <div>
