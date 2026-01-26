2b92904cf328d64605dd0b1bd3da61c7e932e450
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:21:39 +0000

    Jan 26, 2026, 5:51 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -6,7 +6,8 @@ export default function Navbar(){
         items-center *:items-center text-white font-semibold'>
             <div>
                 <div>Home</div>
-                 <button className='bg-emerald-800 active:scale-95 active:bg-emerald-500 px-2 py-1 rounded-md ml-4 active:text-slate-50'
+                 <button onClick={}
+                 className='bg-emerald-800 active:scale-95 active:bg-emerald-500 px-2 py-1 rounded-md ml-4 active:text-slate-50'
                  >Return</button>
             </div>
             <div>
