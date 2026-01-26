c0a2480728b3732453ed30512f72106e0139b6d0
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:04:35 +0000

    Jan 26, 2026, 5:34 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(){
         <div className='flex *:flex bg-emerald-400 p-4 justify-between *:justify-between *:gap-4 text-white'>
             <div>
                 <div>Home</div>
-                 <button className='bg-emerald-800 active:scale-95 active:bg-emerald-600 px-2 py-1'
+                 <button className='bg-emerald-800 active:scale-95 active:bg-emerald-600 px-2 py-1 rounded-md ml-4'
                  >Return</button>
             </div>
             <div>
