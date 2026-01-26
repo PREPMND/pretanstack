cc5a1bf7f5b69d0451d0c090c2e26b7bf414a263
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:22:52 +0000

    Jan 26, 2026, 5:52 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -13,10 +13,10 @@ export default function Navbar(){
             </div>
             <div>
                 <div>
-                    Movies
+                    <button onClick={()=>navigate('/movie')} className='bg-emerald-800 active:scale-95 active:bg-emerald-500 px-2 py-1 rounded-md ml-4 active:text-slate-50'>Movies</button>
                 </div>
                 <div>
-                    Series
+                    <button onClick={()=>navigate('/series')} className='bg-emerald-800 active:scale-95 active:bg-emerald-500 px-2 py-1 rounded-md ml-4 active:text-slate-50'>Series</button>
                 </div>
                 <div>
                     About
