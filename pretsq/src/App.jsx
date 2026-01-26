f0c8f2084ff3864826d20d3fd97c8456b6ae3a77
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-26 12:16:12 +0000

    Jan 26, 2026, 5:46 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/navbar"
+import NavRoutes from "./routing/routing"
 
 export default function App(){
     return(
         <>
         <Navbar/>
-        <Nav
+        <NavRoutes/>
         </>
     )
 }
