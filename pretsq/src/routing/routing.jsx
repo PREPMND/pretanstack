612a7cae43cffbfc6b2919fe01cba3082778b1ff
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 06:33:42 +0000

    Jan 27, 2026, 12:03 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 
 import Movie from '../pages/Movie.jsx'
-import Series from '../pages/Series.jsx'
+import Series from '../pages/Favourites.jsx'
 import About from '../pages/About.jsx'
 export default function NavRoutes(){
     return(
