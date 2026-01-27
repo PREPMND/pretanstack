3a987b4452eec74b9a04544ac8308eb7d501840c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 07:22:02 +0000

    Jan 27, 2026, 12:52 PM

--- pretsq/src/api/axios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const APIKEY=import.meta.env.KEY;
-const api=axios.create({
-    baseURL:'https://api.themoviedb.org/3/movie/popular?api_key='+APIKEY+'&language=en-US&page=1'
-});
-export function fetchPopularMovies(){
-    return api.get('');
-}
-export default api;
