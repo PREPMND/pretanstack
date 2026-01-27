045d4329208c6a1b0e9cfae1b86b6c09b3b6ffd2
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 07:26:58 +0000

    Jan 27, 2026, 12:56 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Navbar(){
     let navigate=useNavigate();
-    const APIKEY=import.meta.env.KEY;
+    const APIKEY=import.meta.env.VITE_TMBD_KEY;
     console.log({APIKEY});
 
     return(
