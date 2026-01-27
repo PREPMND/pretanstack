05ad71c378b5743a95079da2fa1f14e1135f3c9f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 07:20:32 +0000

    Jan 27, 2026, 12:50 PM

--- pretsq/src/api/popularapi.jsx
+++ pretsq/src/api/popularapi.jsx
@@ -4,7 +4,7 @@ export function getPopularMovies(){
     let url='https://api.themoviedb.org/3/movie/popular?api_key='+APIKEY+'&language=en-US&page=1';
     async function getPopularMovies(){
         try{
-            const response= await 
+            const response= await  
         }
     }
 }
