0510fae19cac7287b436369364aa8da33c7d1ab4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 07:44:27 +0000

    Jan 27, 2026, 1:14 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -14,7 +14,7 @@ function Popular() {
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      {data?.slice(0).map((movie) => (
+      {data?.slice(0, 10).map((movie) => (
         <div key={movie.id}>
           <img
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
