fe091c2f7285b64098f960693459ccb9895f6caa
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 06:35:52 +0000

    Jan 27, 2026, 12:05 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { Routes , Route } from 'react-router-dom'
 
 import Movie from '../pages/Movie.jsx'
 import Favourites from '../pages/Favourites.jsx'
