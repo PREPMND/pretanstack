d72f5f201a5f081f53fbd04221c73f11bcafbfcf
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 07:40:29 +0000

    Jan 27, 2026, 1:10 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -4,6 +4,7 @@ import Movie from '../pages/Movie.jsx'
 import Favourites from '../pages/Favourites.jsx'
 import About from '../pages/About.jsx'
 import TrendingMovie from '../pages/trending.jsx'
+import Popular from '../components/popular.jsx'
 export default function NavRoutes(){
     return(
         <>
@@ -13,7 +14,7 @@ export default function NavRoutes(){
                 <Route path="/favourites" element={<Favourites />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/trending/movies" element={<TrendingMovie />}></Route>
-                <Route path="/" element=
+                <Route path="/" element={Popular}
             </Routes>
     
         </>
