c046f52dbd504c969a1b51254d1248590290ef2c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-27 06:13:53 +0000

    Jan 27, 2026, 11:43 AM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar"
 import NavRoutes from "./routing/routing"
 import { BrowserRouter } from "react-router-dom"
+import {}
 export default function App(){
     return(
         <>
