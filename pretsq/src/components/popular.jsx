6922692edb1ae1b6672f64a421cc16772d132626
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 11:28:14 +0000

    Jan 28, 2026, 4:58 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -18,7 +18,7 @@ function Popular() {
 
   return (
     <>    
-    <button className="ml-3 mt-2 font-mono hover:bg-amber-200 hover:border-amber-500">Popular Movies</button>
+    <button className="ml-3 mt-2 font-mono hover:bg-amber-200 hover:border-amber-500 px-2 py-1 rounded">Popular Movies</button>
     <div className="flex overflow-x-scroll overflow-y-hidden mb-5   mt-2">
       {data?.slice(0, 14).map((movie) => (
         <div className="flex-nowrap md:w-[220px] flex-shrink-0 px-3"
