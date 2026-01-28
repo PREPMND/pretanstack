4e6b59c11480d1d9f3457f148d561b19f4871a9f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 11:03:44 +0000

    Jan 28, 2026, 4:33 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -15,7 +15,7 @@ function Popular() {
     return <p>Something went wrong</p>;}
 
   return (
-    <div className="flex flex- overflow-x-scroll  gap-4">
+    <div className="flex flex-nowrap overflow-x-scroll  gap-4">
       {data?.slice(0, 10).map((movie) => (
         <div className=""
         key={movie.id}>
