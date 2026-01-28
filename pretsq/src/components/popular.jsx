6cd5cfb93bd0ce63c1f2ae85716da42a06a42aa7
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 13:42:06 +0000

    Jan 28, 2026, 7:12 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -23,7 +23,7 @@ function Popular() {
     <button className="ml-3 mt-2 font-mono hover:bg-amber-200 hover:border-amber-500 px-2 py-1 rounded active:scale-95">Popular Movies</button>
     <button onClick={()=>setnum(prev=>prev+1)}
     className="ml-3 mt-2 font-mono hover:bg-amber-200 hover:border-amber-500 px-2 py-1 rounded active:scale-95">More Popular Movies</button>
-    <div className="grid grid-flow-col auto-cols-[120px] md:auto-cols-[180px] gap-2 overflow-y-hidden overflow-x-auto ">
+    <div className="grid grid-flow-col auto-cols-[120px] md:auto-cols-[180px] gap-2 mt-3  overflow-y-hidden overflow-x-auto ">
       {data?.slice(0, 10*num).map((movie) => (
         <div className=""
           key={movie.id}>
