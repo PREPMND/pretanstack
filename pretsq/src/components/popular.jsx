7e588a8c152b9dc6dec16474bd41e1ba82868af3
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 14:21:08 +0000

    Jan 28, 2026, 7:51 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -26,7 +26,7 @@ function Popular() {
     <button onClick={()=>navigate("/movie")} 
     className="ml-3 mt-2 bg-amber-400 md:text-[15px] text-[12px] hover:bg-amber-300  text-neutral-900 px-2 py-1 rounded active:scale-95">Popular Movies</button>
     <button onClick={()=>setnum(prev=>prev+1)}
-    className="ml-3 mt-2 md:text-[16px] text-[12px] hover:bg-amber-300 hover:border-amber-500 text-neutral-900 bg-amber-400 px-2 py-1 rounded active:scale-95">More Popular Movies</button>
+    className="ml-3 mt-2 md:text-[15px] text-[12px] hover:bg-amber-300 hover:border-amber-500 text-neutral-900 bg-amber-400 px-2 py-1 rounded active:scale-95">More Popular Movies</button>
     <div className="grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 ">
       {data?.slice(0, 10*num).map((movie) => (
         <div className=""
