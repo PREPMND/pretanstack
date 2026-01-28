8a2ba7d60b0e70c5eb02cd25c53a19ba5898102c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 16:41:27 +0530

    Jan 28, 2026, 4:41 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -18,6 +18,7 @@ function Popular() {
 
   return (
     <>    
+    <div></div>
     <div className="flex  overflow-x-scroll overflow-y-hidden mb-5  gap-4">
       {data?.slice(0, 10).map((movie) => (
         <div className="flex-nowrap  flex-shrink-0"
