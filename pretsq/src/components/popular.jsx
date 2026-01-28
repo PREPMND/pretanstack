a9921851686354ed2d4cf887cd9fce6044d57ea1
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 11:02:37 +0000

    Jan 28, 2026, 4:32 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -15,9 +15,10 @@ function Popular() {
     return <p>Something went wrong</p>;}
 
   return (
-    <div className="flex  overflow-x-scroll gap-4">
+    <div className="flex   gap-4">
       {data?.slice(0, 10).map((movie) => (
-        <div key={movie.id}>
+        <div 
+        key={movie.id}>
           <img className='md:h-[260px] md:w-[260px]'
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
