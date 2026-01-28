bf5c175e81133744ec611ce20b7bb5d3810dd7eb
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 11:41:40 +0000

    Jan 28, 2026, 5:11 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -22,7 +22,7 @@ function Popular() {
     <button className="ml-3 mt-2 font-mono hover:bg-amber-200 hover:border-amber-500 px-2 py-1 rounded active:scale-95">More Popular Movies</button>
     <div className="flex overflow-x- overflow-y-hidden mb-5   mt-2">
       {data?.slice(0, 14).map((movie) => (
-        <div className="flex-nowrap w-[44vw] max-w-  flex-shrink-0 px-3"
+        <div className="flex-nowrap w-[44vw] max-w-64  flex-shrink-0 px-3"
           key={movie.id}>
           <img className='h-[90%] hover:scale-105 transition-transform duration-200 ease-in-out object-cover relative rounded scale '
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
