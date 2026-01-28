b0cdaf487fdb30b9933b682fdd820c157ea8c95d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 11:07:13 +0000

    Jan 28, 2026, 4:37 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -19,7 +19,7 @@ function Popular() {
       {data?.slice(0, 10).map((movie) => (
         <div className="flex-nowrap  flex-shrink-0"
         key={movie.id}>
-          <img className='md:h-[280px] md:w-[260px] object-cover'
+          <img className='md:h-full md:w-[260px] object-cover'
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
           />
