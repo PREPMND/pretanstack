e901a82dc1625eb8df53bcec5b7b201bb9f52430
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-28 13:47:18 +0000

    Jan 28, 2026, 7:17 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -28,7 +28,7 @@ function Popular() {
         <div className=""
           key={movie.id}>
         <div className="w-full aspect-[2/3] place-items-center"> 
-          <img className='hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded object-top h-full'
+          <img className='hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded h-full'
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
           />
