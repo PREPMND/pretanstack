e7139f6dc021586133cd3f245df8dd7258c6655c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:55:20 +0000

    Jan 29, 2026, 7:25 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -11,7 +11,7 @@ export default function TopRated() {
         queryFn: () => fetchTopRated(),
     });
     console.log(data);
-    if(data.top_)
+    if(data.top_rated)
     if (isLoading) return (<>
 
         <p className='h-screen flex justify-center items-center '>Loading...</p></>)
