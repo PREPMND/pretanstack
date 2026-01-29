768943741db2dee742100b41c3a41f42b4442d05
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:50:35 +0000

    Jan 29, 2026, 7:20 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -21,7 +21,7 @@ export default function TopRated() {
         return <p>Something went wrong</p>;
     }
     return (
-        <div className='bg-neutral-900 f mt-3 '>
+        <div className='bg-neutral-900 mt-3 '>
             <div className='bg-neutral-900 flex justify-center flex-col w-full '>
                 <button 
                     className="ml-3 mt-2 block mx-auto md:text-[18px] font-[600] text-[14px] bg-white/20 hover:bg-white/30 text-white backdrop-blur-sm px-3 py-2 rounded active:scale-95">Top Rated</button>
