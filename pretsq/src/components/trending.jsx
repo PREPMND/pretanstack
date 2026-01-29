c0c9f444357a7b0ef0b6bfa3765607c09e195813
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:32:19 +0000

    Jan 29, 2026, 7:02 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -22,6 +22,7 @@ export default function TopRated() {
     }
     return (
         <div  className='bg-neutral-900'>
+            
         <div className="grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 ">
             {data?.slice(0, 10 * num).map((movie) => (
                 <div className=""
