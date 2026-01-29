5c5f4937de00cfcee6c4dd53d2a26da0ac4c9257
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 14:03:08 +0000

    Jan 29, 2026, 7:33 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -37,7 +37,7 @@ export default function TopRated() {
                         </div>
                         <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
                         <h3 className={`truncate mt-[6px] md:pb-1 font-Inter text-[11px]
-                         font- 
+                        md:text-[11px] font- 
                         text-white`}>{movie.vote_average}</h3>
                     </div>
 
