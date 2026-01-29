97cb3e193572eaea1e2fd001899a231dbc618595
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:12:03 +0000

    Jan 29, 2026, 6:42 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -15,9 +15,11 @@ export default function NavRoutes(){
                 <Route path="/favourites" element={<Favourites />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/trending/movies" element={<TrendingMovie />}></Route>
-                <Route path="/" element={<>
+                <Route path="/" element={
+                <>
                 <TopRated/>
-                <Popular/></>}/>
+                <Popular/>
+                </>}/>
             </Routes>
     
         </>
