aa2fd4b1db0230650acbdb474d66ed13740b182a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:03:34 +0000

    Jan 29, 2026, 6:33 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -19,5 +19,19 @@ function Popular() {
 
     return <p>Something went wrong</p>;
   }
-  
+  <div className="grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 ">
+      {data?.slice(0, 10*num).map((movie) => (
+        <div className=""
+          key={movie.id}>
+        <div className="w-full bg-neutral-900 aspect-[2/3] place-items-center"> 
+          <img className='hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded h-full '
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+        </div> 
+          <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
+        </div>
+
+      ))}
+    </div>
 }
