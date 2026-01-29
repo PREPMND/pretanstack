469b80bc661fd810e45396d7b212c33a145c3eb6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-29 13:55:27 +0000

    Jan 29, 2026, 7:25 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -10,7 +10,7 @@ export default function TopRated() {
         queryKey: ["top-rated"],
         queryFn: () => fetchTopRated(),
     });
-    console.log(data);
+    console.log(data_toprated);
     if(data.top_rated)
     if (isLoading) return (<>
 
