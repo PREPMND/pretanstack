8b8b79f90eec985098f7a35d5a695607630760ca
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-30 13:59:33 +0000

    Jan 30, 2026, 7:29 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchPopularMovies } from "../api/popularapi.jsx";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { fetchTopRated } from "../api/trendingapi.jsx";
+import { fetchTopRated } from "../api/topratedapi.jsx";
 function Popular() {
   const navigate= useNavigate();
   const [num,setnum]=useState(1);
