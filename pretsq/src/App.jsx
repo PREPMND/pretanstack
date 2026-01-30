668aee1e65189f52fa538d047515b2bc38f175da
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-30 14:22:02 +0000

    Jan 30, 2026, 7:52 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/navbar"
 import NavRoutes from "./routing/routing"
-import Popular from "./components/popular"
 import { BrowserRouter } from "react-router-dom"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"    
 export default function App(){
