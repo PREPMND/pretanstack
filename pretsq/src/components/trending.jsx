7953db5357b417e028c9a425e7556a141ab6ff25
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-30 14:05:03 +0000

    Jan 30, 2026, 7:35 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -1,7 +1,7 @@
 import { fetchTrending } from "../api/trendingapi";
 import { useQuery } from "@tanstack/react-query";
 export default function Trending(){
-    useQuery({
+    const { }useQuery({
         queryKey:"trending",
         queryFn:()=>{fetchTrending}
     })
