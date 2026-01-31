a0be131c9e27ba05f3162a514664efb35ec7d1a8
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 07:57:29 +0000

    Jan 31, 2026, 1:27 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -2,8 +2,9 @@ import Navbar from "./components/navbar"
 import NavRoutes from "./routing/routing"
 import { BrowserRouter } from "react-router-dom"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"    
+import { useState } from "react"
 export default function App(){
-    const [movieHovered , setmovieHovered]=
+    const [movieHovered , setmovieHovered]=useState
     return(
         <>
         <QueryClientProvider client={new QueryClient()}>
