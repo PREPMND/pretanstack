027ec69956b729cb3e57734a160d20d3bc01c820
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 09:27:20 +0000

    Jan 31, 2026, 2:57 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -31,20 +31,37 @@ function Popular() {
     <button onClick={()=>setnum(prev=>prev+1)}
     className="ml-3 mt-2 m-auto md:text-[15px] font-[600] text-[13px] bg-white/20 hover:bg-white/30 text-white backdrop-blur-sm px-2 py-1 rounded active:scale-95">More Popular Movies</button>
     </div>
-    <div className="grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[180px] gap-2 md:gap-4 overflow-y-hidden mt-4 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 ">
-      {data?.slice(0, 10*num).map((movie) => (
-        <div className=""
-          key={movie.id}>
-        <div className="w-full bg-neutral-900 aspect-[2/3] place-items-center"> 
-          <img className='hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded h-full '
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-        </div> 
-          <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
-        </div>
-
-      ))}
+    <div className={`grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 pt-2 
+            ${selected?"pointer-events-none":""}`}>
+                {data?.slice(0, 19).map((movie) => 
+                    <div className=""
+                        key={movie.id}>
+                        <div 
+                            onMouseEnter={() => setmovieHovered(movie.id)}
+                            onMouseLeave={() => setmovieHovered(null)}
+                            className={`w-full relative bg-neutral-900 aspect-[2/3] overflow-hidden group `}
+                        >
+                            <img
+                                className="group-hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded h-full w-full"
+                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                alt={movie.title}
+                            />
+                            <div
+                                className={`absolute inset-0 z-30 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movieHovered === movie.id ? "opacity-100" : "opacity-0"}`}
+                            >
+                                <button
+                                    onClick={() => setselected(movie)}
+                                    className={`bg-black/70 hover:bg-black/70 px-4 py-2 rounded text-white text-sm flex items-center gap-2`}
+                                >
+                                    <span className="text-red-500">▶</span> Play
+                                </button>
+                            </div>
+                        </div>
+                        <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
+                        <h3 className={`truncate mt-[6px] md:pb-1 font-Inter text-[11px] md:text-[13px] text-white`}>{movie.vote_average}</h3>
+                    </div>
+                )}
+            </div>
     </div>
     {selected && (
                 <div onClick={() => setselected(null)}
