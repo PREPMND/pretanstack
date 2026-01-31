d3385ddd6fe05136440e161a72359f742ec716e4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 09:50:58 +0000

    Jan 31, 2026, 3:20 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -66,7 +66,7 @@ function Popular(props) {
       </div>
       {selected && (
         <div onClick={() => setselected(null)}
-          className="fixed inset-0 bg-black/80 flex items-center justify-center">
+          className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center">
           <div onClick={(e) => e.stopPropagation()}
             className="relative w-[90%] ">
             <img
