6305458577ac89d7184831683221ee4e23dcecb6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:17:25 +0000

    Jan 31, 2026, 1:47 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -28,7 +28,7 @@ export default function Trending(props) {
                 {data?.slice(0, 19).map((movie) => (
                     <div className=""
                         key={movie.id}>
-                        <div className="w-full rekative z-30 bg-neutral-900 aspect-[2/3] place-items-center">
+                        <div className="w-full relative z-30 bg-neutral-900 aspect-[2/3] place-items-center">
                             <img 
                             onMouseEnter={()=>setmovieHovered(movie.id)}
                             onMouseLeave={()=>setmovieHovered(null)}
