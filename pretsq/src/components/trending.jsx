01ce1ce2a6e6536fb4bd920166362f7997b09590
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 12:21:38 +0000

    Jan 31, 2026, 5:51 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -2,7 +2,7 @@ import { fetchTrending } from "../api/trendingapi.jsx";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { movie}
+import { Movie}
 export default function Trending(props) {
     const [movieHovered , setmovieHovered]=useState(null);
     const [selected, setselected] = useState(null);
