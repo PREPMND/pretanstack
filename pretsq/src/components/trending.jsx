677ff49cbb8f0d03e87f56804b94756b652ff35e
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:36:58 +0000

    Jan 31, 2026, 2:06 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -38,7 +38,7 @@ export default function Trending(props) {
                                 alt={movie.title}
                             />
                             <div onClick={() => setselected(movie)}
-                            className={`absolute inset-0 z-30 ${movieHovered== movie.id?"flex":"hidden"} items-center justify-center text-white pointer-events-none text-[17px]`}><span className="text-red-500 pr-[3px]">▶  </span> Play</div>
+                            className={`absolute inset-0 z-15 ${movieHovered== movie.id?"flex":"hidden"} items-center justify-center text-white pointer-events-none text-[17px]`}><span className="text-red-500 pr-[3px]">▶  </span> Play</div>
                         </div>
                         <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
                         <h3 className={`truncate mt-[6px] md:pb-1 font-Inter text-[11px]
