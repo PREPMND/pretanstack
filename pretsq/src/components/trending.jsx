e3d5c4a86385f3c9753f72da762cf150df981bf9
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 09:34:38 +0000

    Jan 31, 2026, 3:04 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export default function Trending(props) {
     const [movieHovered , setmovieHovered]=useState(null);
     const [selected, setselected] = useState(null);
-    const navigate=useNavigate
+    const navigate=useNavigate();
     const { data, isPending, error } = useQuery({
 
         queryKey: ["trending"],
