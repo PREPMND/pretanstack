72ffc97a0b6961beba52f0ae38f1e47c90973e13
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:23:07 +0000

    Jan 31, 2026, 1:53 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -40,7 +40,7 @@ export default function Trending(props) {
                             <div 
                             className={`absolute inset-0 z-30 ${movieHovered== movie.id?"flex":"hidden"} items-center justify-center text-white`}>Play</div>
                         </div>
-                        <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.id}</h3>
+                        <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
                         <h3 className={`truncate mt-[6px] md:pb-1 font-Inter text-[11px]
                         md:text-[13px] 
                         text-white`}>{movie.vote_average}</h3>
