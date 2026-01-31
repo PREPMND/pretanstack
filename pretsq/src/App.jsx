0d960b3df457e6169cc5d6e3d5b4b331033a0663
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 12:19:46 +0000

    Jan 31, 2026, 5:49 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -20,7 +20,7 @@ export default function App(){
 export function ChangeTitle(moviename){
     return moviename
     .toLowercase()
-    .replace(/[^a-z0-9\s-]/)
+    .replace(/[^a-z0-9\s-]/g,"")
 
 
 }
