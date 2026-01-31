7e115d5341195d46c32ac9fa1a54081ea7f70b29
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:14:04 +0000

    Jan 31, 2026, 1:44 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -38,7 +38,7 @@ export default function Trending(props) {
                                 alt={movie.title}
                             />
                             <div 
-                            className={`relative z-20 ${movieHovered==movie.id?"flex":"hidden"} items-center justify-center text-white`}>Play</div>
+                            className={`relative z-40 ${movieHovered==movie.id?"flex":"hidden"} items-center justify-center text-white`}>Play</div>
                         </div>
                         <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{`${setmovieHovered?"yes":"no"}`}</h3>
                         <h3 className={`truncate mt-[6px] md:pb-1 font-Inter text-[11px]
