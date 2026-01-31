a53dff9696b1743c423295994ecbc21fda0aa130
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 07:58:29 +0000

    Jan 31, 2026, 1:28 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -10,7 +10,7 @@ export default function App(){
         <QueryClientProvider client={new QueryClient()}>
         <BrowserRouter>
            <Navbar/>
-           <NavRoutes movieHovered={movieHovered} />
+           <NavRoutes movieHovered={movieHovered} setmovieHovered={}/>
            
         </BrowserRouter>
         </QueryClientProvider>
