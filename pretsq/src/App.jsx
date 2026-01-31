35c06390eb391c4a33220c259b3aaae8008cb03f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 07:57:09 +0000

    Jan 31, 2026, 1:27 PM

--- pretsq/src/App.jsx
+++ pretsq/src/App.jsx
@@ -3,7 +3,7 @@ import NavRoutes from "./routing/routing"
 import { BrowserRouter } from "react-router-dom"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"    
 export default function App(){
-    const [move]
+    const [movieHovered , ]
     return(
         <>
         <QueryClientProvider client={new QueryClient()}>
