51c20e86471e8e750a82049de09492a5d41b5d47
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:04:27 +0000

    Jan 31, 2026, 1:34 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -30,8 +30,8 @@ export default function Trending(props) {
                         key={movie.id}>
                         <div className="w-full z-30 bg-neutral-900 aspect-[2/3] place-items-center">
                             <img 
-                            
-                            
+                            onMouseEnter={()=>setmovieHovered(true)}
+                            onMouseLeave={()=>setmovieHovered(false)}
                             onClick={() => setselected(movie)}
                             className='hover:scale-105 transition-transform duration-200 ease-in-out object-cover rounded h-full '
                                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
