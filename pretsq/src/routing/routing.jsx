d2253521fd60e1dcacedf6621d6e4a60cfe2e34a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 10:02:42 +0000

    Jan 31, 2026, 3:32 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -10,7 +10,7 @@ import Trending from '../components/trending.jsx'
 import TOPrated from '../pages/toprated.jsx'
 import TrendingMovie from '../pages/trendingmovie.jsx'
 export default function NavRoutes(props){
-    const [selectedglobal , set]
+    const [selectedglobal , setselectedglobal]
     return(
         <>
         
