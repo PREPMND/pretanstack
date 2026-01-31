3d320ccc17dddd11e2c0dbe60b737eca95e3b664
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 10:07:43 +0000

    Jan 31, 2026, 3:37 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -60,7 +60,7 @@ export default function Trending(props) {
                 )}
             </div>
             {selected && (
-                <div onClick={() => setselected(null)}
+                <div onClick={() => {setselected(null)}}
                     className="fixed inset-0 bg-black/80 flex items-center justify-center">
                     <div onClick={(e) => e.stopPropagation()}
                         className="relative w-[90%] ">
