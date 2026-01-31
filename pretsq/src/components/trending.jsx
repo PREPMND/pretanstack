76bfeb9c6bd4c93f10d4024123945f2383d5bbf8
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 08:01:55 +0000

    Jan 31, 2026, 1:31 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -36,7 +36,7 @@ export default function Trending(props) {
                             />
                             <div 
                             onMouseEnter={()=>setmovieHovered(true)}
-                            onMouseLeave={()}
+                            onMouseLeave={()=>setmovieHovered(t)}
                             className="relative z-20 flex items-center justify-center text-white hidden">Play</div>
                         </div>
                         <h3 className="truncate mt-[6px] md:pb-1 font-Inter font-semibold text-slate-50">{movie.title}</h3>
