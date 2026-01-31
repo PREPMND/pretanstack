39da2d4a4882fa47933dc4f010f8fc6e3df62ae6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-01-31 10:08:17 +0000

    Jan 31, 2026, 3:38 PM

--- pretsq/src/components/trending.jsx
+++ pretsq/src/components/trending.jsx
@@ -48,6 +48,7 @@ export default function Trending(props) {
                             >
                                 <button
                                     onClick={() => {
+                                        setselectedglobal(movie)
                                         setselected(movie)}}
                                     className={`bg-black/70 hover:bg-black/70 px-4 py-2 rounded text-white text-sm flex items-center gap-2`}
                                 >
