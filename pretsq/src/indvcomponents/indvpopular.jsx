a2ec01d14b2f508881c201d8da07ed84fb1d845e
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 13:05:07 +0000

    Feb 1, 2026, 6:35 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query"
-import { fetchPopularMovies } from "../src/api/popularapi"
+import { fetchPopularMovies } from "../api/popularapi"
 export default function IndvPopular(props){
     const { selected , setselected}=props;
     const { data , isLoading , error}=useQuery({
