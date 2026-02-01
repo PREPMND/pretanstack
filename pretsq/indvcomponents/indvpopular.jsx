f7b214ceea04e46360422d13f7e725f9d1f198d6
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 13:00:33 +0000

    Feb 1, 2026, 6:30 PM

--- pretsq/indvcomponents/indvpopular.jsx
+++ pretsq/indvcomponents/indvpopular.jsx
@@ -12,7 +12,7 @@ export default function IndvPopular(props){
     return(
         <>
         <div>
-            {selected.}
+            {selected.title}
         </div>
        <div className="text-black">HEllo</div>
 
