d8591b0b71d2853a10e39a1e2a6f1f439d698fcb
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 12:14:58 +0000

    Feb 1, 2026, 5:44 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -27,7 +27,7 @@ export default function NavRoutes(props){
                 <Route path="/" element={
                 <>
                 <div className='thebody'>
-                    <Popular selectedglobal={selectedglobal} setselectedglobal={setselectedglobal} selected={selected } setselected />
+                    <Popular selectedglobal={selectedglobal} setselectedglobal={setselectedglobal} selected={selected } setselected={setselected} />
                     <TopRated selectedglobal={selectedglobal} setselectedglobal={setselectedglobal}/>
                     <Trending selectedglobal={selectedglobal} setselectedglobal={setselectedglobal}/>
                 </div>
