9404aab863ee968a0c1b2a94c25f3f98f0b63fbb
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 12:13:48 +0000

    Feb 1, 2026, 5:43 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -7,7 +7,7 @@ function Popular(props) {
   const navigate = useNavigate();
   const [movieHovered , setmovieHovered]=useState(null);
   const [selected, setselected] = useState(null)
-  const {selected}
+  const {selected , setselected }
   const [num, setnum] = useState(1);
   const { data, isLoading, error } = useQuery({
     queryKey: ["popular-movies"],
