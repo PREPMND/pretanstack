8553e78863e264c2edefda526c1a7fb657f5d678
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 12:09:08 +0000

    Feb 1, 2026, 5:39 PM

--- pretsq/indvcomponents/popular.jsx
+++ pretsq/indvcomponents/popular.jsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query"
+import { fetchPopularMovies } from "../src/api/popularapi"
 export default function (){
     const { data , isLoading , error}=useQuery({
         queryKey:["IndPopular"],
-        queryFn:()=>
+        queryFn:()=>fetchPopularMovies()
 
     })
     return(
