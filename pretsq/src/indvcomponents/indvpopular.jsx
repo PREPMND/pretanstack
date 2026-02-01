65807672c61a742451049b99b1175db6bd4e1814
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 14:40:35 +0000

    Feb 1, 2026, 8:10 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -5,7 +5,7 @@ export default function IndvPopular(props){
     const { id }=useParams();
     const { selected , setselected}=props;
     const { data : movie , isLoading , error}=useQuery({
-        queryKey:["IndPopular"],
+        queryKey:["IndPopular" , id],
         queryFn:()=>fetchPopularMovies(),
         refetchOnWindowFocus:true,
         staleTime:24*60*60*1000,
