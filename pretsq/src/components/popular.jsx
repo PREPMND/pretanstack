be80939b668c04af92908f0dbeac93e4ae123c5a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 14:59:55 +0000

    Feb 1, 2026, 8:29 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -78,7 +78,9 @@ function Popular(props) {
             
             <button
               className="absolute inset-0 flex items-center justify-center text-white text-3xl"
-              onClick={(e)=>navigate(`/popular/${ChangeTitle(selected.title)}`)}
+              onClick={(e)=>{
+                
+                navigate(`/popular/${ChangeTitle(selected.title)}`)}}
             >
               ▶
             </button>
