a82cb00aa73e0dc3c2b0e575966ee25a0b810e1c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 14:37:34 +0000

    Feb 1, 2026, 8:07 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -78,11 +78,10 @@ function Popular(props) {
             
             <button
               className="absolute inset-0 flex items-center justify-center text-white text-3xl"
-              
+              onC
             >
               ▶
             </button>
-            </Link>
           </div>
         </div>
       )}
