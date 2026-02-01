fb90b2b0259c3f365f439ca51b187b12ac3b0fe7
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 15:29:54 +0000

    Feb 1, 2026, 8:59 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -25,6 +25,7 @@ export default function NavRoutes(props){
                 <Route path="/toprated" element={<TOPrated/>}/>
                 <Route path="/trending" element={<TrendingMovie/>}/>
                 <Route path="/popular/:id" element={<IndvPopular selected={selected } setselected={setselected}/>}/>
+                <R
                 <Route path="/" element={
                 <>
                 <div className='thebody'>
