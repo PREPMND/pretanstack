dd7e4ecf1fd438ff51e2c5be9940ce68b3acfef4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 15:19:09 +0000

    Feb 1, 2026, 8:49 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -18,7 +18,7 @@ export default function NavRoutes(props){
     const { id }=useParams();
     return(
         <>
-        <Context.Provider value
+        <Context.Provider value={{}}
             <Routes>
                 <Route path="/movie" element={<Movie />} />
                 <Route path="/favourites" element={<Favourites />} />
