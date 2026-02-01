b70f237f94bfcc3716ae7e030379a874879e9850
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 12:10:35 +0000

    Feb 1, 2026, 5:40 PM

--- pretsq/indvcomponents/popular.jsx
+++ pretsq/indvcomponents/popular.jsx
@@ -5,7 +5,7 @@ export default function (){
         queryKey:["IndPopular"],
         queryFn:()=>fetchPopularMovies(),
         refetchOnWindowFocus:true,
-        staleTime:24*60
+        staleTime:24*60*60*1000,
 
     })
     return(
