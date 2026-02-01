bfa0213262458c59c13c642391778b270fc57696
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-01 12:40:30 +0000

    Feb 1, 2026, 6:10 PM

--- pretsq/src/components/popular.jsx
+++ pretsq/src/components/popular.jsx
@@ -77,7 +77,7 @@ function Popular(props) {
             />
             <Link
             to={`/popular/${ChangeTitle(selected.title)}`}
-            target=""
+            target="_blank"
             >
             <button
               className="absolute inset-0 flex items-center justify-center text-white text-3xl"
