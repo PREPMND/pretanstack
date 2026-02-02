7fdaf12c78e0c25c17dde11068b816d0e9f9e219
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:01:59 +0000

    Feb 2, 2026, 6:31 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -15,7 +15,7 @@ export default function IndvPopular(props){
             </div>
             <div>
                 <img 
-                src={``}
+                src={`selected.`}
             </div>
         </div>
        <div >HEllo</div>
