70a99c581f8e25d7e91ab36403e5bc7b89ff8402
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:11:28 +0000

    Feb 2, 2026, 6:41 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -9,7 +9,7 @@ export default function IndvPopular(props){
         <>
         
         <div className="">
-            <div className="font-Inter text-2xl font-">
+            <div className="font-Inter text-2xl font-light">
                 <div>{selected.title}</div>
                 
             </div>
