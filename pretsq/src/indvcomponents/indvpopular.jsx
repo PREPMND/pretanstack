d2b260ae7f2e1b7bbf4c1a716239b9425a654e75
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:16:01 +0000

    Feb 2, 2026, 6:46 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -22,7 +22,7 @@ export default function IndvPopular(props){
             </div>
             <div>
                 <div onClick={()=>{
-                    window.open({`htt`})
+                    window.open(`https`)
                 }}
                 ></div>
             </div>
