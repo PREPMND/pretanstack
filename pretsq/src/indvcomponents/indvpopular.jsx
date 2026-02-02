ff31149322fc77339530b42544cad37775de9808
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:23:11 +0000

    Feb 2, 2026, 6:53 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -14,7 +14,7 @@ export default function IndvPopular(props){
                 <div>{selected.title}</div>
                 
             </div>
-            <div className="aspect-[3/2] h-fit mt-1 flex justify-center overflow-hidden">
+            <div className="aspect-[3/2] h-fit mt-1 flex justify-center overflow-hidden relative">
                   
                 <img className="rounded-lg hover:scale-[1.02] transition-transform duration-200 ease-in-out object-cover h-[50%] w-[50%] "
                 src={`https://image.tmdb.org/t/p/original${selected.backdrop_path}`}
