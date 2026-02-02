81a5924d6e4a9cd6f3b1cc4c51a26d91cfb7f03c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:20:12 +0000

    Feb 2, 2026, 6:50 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -9,7 +9,7 @@ export default function IndvPopular(props){
     return(
         <>
         
-        <div className="">
+        <div className="flex flex-">
             <div className="font-Inter text-2xl text-center font-light">
                 <div>{selected.title}</div>
                 
