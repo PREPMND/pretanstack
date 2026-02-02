d18274b9a6210fca14e69a064e66330a6fe5e3ca
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-02 13:02:05 +0000

    Feb 2, 2026, 6:32 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -15,7 +15,7 @@ export default function IndvPopular(props){
             </div>
             <div>
                 <img 
-                src={`selected.`}
+                src={`selected.backdrop_path`}
             </div>
         </div>
        <div >HEllo</div>
