087261021b41cbe50871a283f781a2f081a93162
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-03 15:44:14 +0000

    Feb 3, 2026, 9:14 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -28,7 +28,7 @@ export default function IndvPopular(props){
             onClick={()=>{
                 window.open(`https://dorawatch.one/${ChangeTitle(selected.title)}`)
             }}
-            className="mt-3 flex justify-center itrm">
+            className="mt-3 flex justify-center items-center">
                 <Popcorn className="" strokeWidth={0.5} />
                 <div className="text-center font-Inter elastic"
                 
