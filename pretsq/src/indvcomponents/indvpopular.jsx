8c84eebb21bd68a786ec43247b522d01ac2728be
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-03 15:38:19 +0000

    Feb 3, 2026, 9:08 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -28,7 +28,7 @@ export default function IndvPopular(props){
                 onClick={()=>{
                     window.open(`https://dorawatch.one/${ChangeTitle(selected.title)}`)
                 }}
-                ><Popcorn  Movie Player - DoraWatch</div>
+                ><Popcorn /> Movie Player - DoraWatch</div>
             </div>
         </div>
 
