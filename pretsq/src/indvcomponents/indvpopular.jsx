a798de6189b201da1496a51aca8a33a2330bba4f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-03 15:35:53 +0000

    Feb 3, 2026, 9:05 PM

--- pretsq/src/indvcomponents/indvpopular.jsx
+++ pretsq/src/indvcomponents/indvpopular.jsx
@@ -21,7 +21,7 @@ export default function IndvPopular(props){
                 alt={selected.title}/>
               
             </div>
-            <div className="flex justify-center max-w-[70%"
+            <div className="flex justify-center max-w-[70%]"
             >{selected.overview}</div>
             <div className="mt-3">
                 <div className="text-center font-Inter elastic"
