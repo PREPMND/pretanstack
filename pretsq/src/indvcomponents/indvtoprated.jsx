c88577ac173f13bdee4fbe14749597a118c70488
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-04 13:51:30 +0000

    Feb 4, 2026, 7:21 PM

--- pretsq/src/indvcomponents/indvtoprated.jsx
+++ pretsq/src/indvcomponents/indvtoprated.jsx
@@ -24,7 +24,7 @@ export default function IndvToprated(props){
             </div >
             <div className="flex mt-5 m-auto justify-center max-w-[70%]"
             >{selectedtoprated.overview}</div>
-            <div className="text-center text-3xl text-red-100 mt-10 font-[700] mt-5 " >Players</div>
+            <div className="text-center text-3xl text-red-100 mt-10 font-[700]" >Players</div>
             <div className="flex justify-center">
             <div className="flex items-start flex-col justify-center">
             <div 
