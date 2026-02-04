3f614fc6fcd1968c94b2fc2d21376d4c4644ae12
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-04 13:47:30 +0000

    Feb 4, 2026, 7:17 PM

--- pretsq/src/indvcomponents/indvtoprated.jsx
+++ pretsq/src/indvcomponents/indvtoprated.jsx
@@ -9,7 +9,7 @@ export default function IndvToprated(props){
 
     return(
         <>
-        <div className="bg-neutral-900 h-screen md:h-full">
+        <div className="bg-neutral-900 min-h-[calc(100vh-64px)] md:h-full">
         <div className=" font-Inter w-full text-white flex flex-col pl-3">
             <div className="mt-2 text-2xl text-center font-light">
                 <div>{selectedtoprated.title}</div>
