09205ab11c76e1586d97747092f9a0fcb67f3c5b
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-04 13:54:22 +0000

    Feb 4, 2026, 7:24 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -27,7 +27,7 @@ export default function Navbar(){
             </div>
         </div>
         <div>
-            <input type="text" id="search" placeholder=' Search...' className='ml-4 mt-2 mb-1 p-1 pl-2 mx-5 md:block hidden rounded-md text-black
+            <input type="text" id="search" placeholder=' Search...' className='ml-4 mt-2 mb-1 p-1 pl-2 w-[50]mx-5 md:block hidden rounded-md text-black
                  '/>
         </div>
 
