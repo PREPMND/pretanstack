746fd9b03956a8a34b50dc2eb2a04a2f11522895
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-04 14:36:15 +0000

    Feb 4, 2026, 8:06 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -5,6 +5,7 @@ export default function Navbar(props){
     const { selected , setselected}=props;
     const {selectedtoprated, setselectedtoprated} = props
     const { selectedtrending , setselectedtrending}=props;
+    const { selectedglobal}
     const APIKEY=import.meta.env.VITE_TMBD_KEY;
     return(
         <>
