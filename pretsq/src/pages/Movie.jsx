add757619284b5abddebe66f453134c4a96e7831
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:05:24 +0000

    Feb 7, 2026, 9:35 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -29,7 +29,7 @@ export default function Movie(props) {
 
                                 </div>
                                 <div
-                                className={`absolute inset-0 z-30 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movieHovered === movie.id ? "opacity-100" : "opacity-0"}`}
+                                className={`absolute inset-0 z-30 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movies === movie.id ? "opacity-100" : "opacity-0"}`}
                             >
                                 <button
                                     onClick={() => {
