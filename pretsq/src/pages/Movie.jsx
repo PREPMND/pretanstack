c1a2f8b17fc956de016c0cd2632875359c808738
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:56:36 +0000

    Feb 7, 2026, 9:26 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -17,7 +17,7 @@ export default function Movie(){
                         <div className=" "
                         key={movie.id}>
                             <div>
-                                <img className=""
+                                <img className="rounded object-cover"
                                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />   
                                 <h2>{movie.title}</h2>
                                 
