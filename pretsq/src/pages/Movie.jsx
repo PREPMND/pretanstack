bbe9b34e21768f2cbbd5199c17c841c1ded05252
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:22:57 +0000

    Feb 7, 2026, 9:52 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -26,13 +26,13 @@ export default function Movie(props) {
                                 key={movie.id}>
                                 <div
                                     
-                                >   
+                                >   <div>
                                     <img 
                                     onMouseEnter={() => setMovies(movie.id)}
                                     onMouseLeave={() => setMovies(null)}
                                     className="rounded object-cover z-50"
                                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                                    <h2>{movie.title}</h2>
+                                    <h2>{movie.title}</h2></div>
                                     <div
                                 className={`absolute inset-0 z-40 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movies === movie ? "opacity-100" : "opacity-0"}`}
                             >
