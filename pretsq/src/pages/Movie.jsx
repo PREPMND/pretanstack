8e537224d799b447c783ab10135c5831e98bdea1
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:18:02 +0000

    Feb 7, 2026, 9:48 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -11,6 +11,8 @@ export default function Movie(props) {
         queryKey: ["movies"],
         queryFn: () => fetchMovies(),
     })
+    console.log();
+    
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>Error: {error.message}</div>
     return (
