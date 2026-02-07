c687349454dd0417214119b6cac5d3df7cb890f3
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:24:55 +0000

    Feb 7, 2026, 8:54 PM

--- pretsq/src/indvcomponents/error.jsx
+++ pretsq/src/indvcomponents/error.jsx
@@ -1,7 +1,10 @@
 export function Error(){
     return(
         <>
-        
+        <div className='flex flex-col items-center justify-center h-screen gap-4'>
+            <h1 className='text-4xl font-bold text-red-500'>404</h1>
+            <p className='text-lg text-gray-600'>Page Not Found</p>
+        </div>
         </>
            
 
