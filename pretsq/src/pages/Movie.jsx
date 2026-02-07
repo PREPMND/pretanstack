067b3b8a3f7dfd986c074fd2754adfb9b1cf2530
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:46:50 +0000

    Feb 7, 2026, 9:16 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -4,7 +4,7 @@ export default function Movie(){
         <>
         <div>
             <div>
-                data.slice(0,10).map((movie)=>{
+                data?.slice(0,10).map((movie)=>{
                     return(
                         <div key={movie.id}>
                             {movie.title}
