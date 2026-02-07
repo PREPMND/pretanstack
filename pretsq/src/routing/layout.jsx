4597ceef5cd6c47ccf4a8446a1fee12b8e54123d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:35:44 +0000

    Feb 7, 2026, 9:05 PM

--- pretsq/src/routing/layout.jsx
+++ pretsq/src/routing/layout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar.jsx";
 import NavRoutes from "./routing.jsx";
-export default function Layout(props){
-    const [selectedtoprated,setselectedtoprated]=useState(null)
-        const [selectedtrending, setselectedtrending] = useState(null)
-        const [selected, setselected] = useState(null)
-        const [selectedglobal , setselectedglobal]=useState(null);
-    return(
+export default function Layout(props) {
+    const [selectedtoprated, setselectedtoprated] = useState(null)
+    const [selectedtrending, setselectedtrending] = useState(null)
+    const [selected, setselected] = useState(null)
+    const [selectedglobal, setselectedglobal] = useState(null);
+    return (
         <>
-        <Navbar selected={props.selected} setselected={props.setselected} selectedtoprated={props.selectedtoprated} setselectedtoprated={props.setselectedtoprated} selectedtrending={props.selectedtrending} setselectedtrending={props.setselectedtrending} selectedglobal={props.selectedglobal} setselectedglobal={props.setselectedglobal}/>
-        <Outlet/>
+            <Navbar selected={props.selected} setselected={props.setselected} selectedtoprated={props.selectedtoprated} setselectedtoprated={props.setselectedtoprated} selectedtrending={props.selectedtrending} setselectedtrending={props.setselectedtrending} selectedglobal={props.selectedglobal} setselectedglobal={props.setselectedglobal} />
+            <Outlet />
         </>
     )
 }
