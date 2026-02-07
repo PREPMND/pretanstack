d9d33dbae4539443fa58da108f0f2053f5b61a2c
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:34:26 +0000

    Feb 7, 2026, 9:04 PM

--- pretsq/src/routing/layout.jsx
+++ pretsq/src/routing/layout.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/navbar.jsx";
 import NavRoutes from "./routing.jsx";
 export default function Layout(props){
     return(
-        <.
+        <>
+        
+        </>
     )
 }
