ad26033ff57198592a5e5a119351ab419b741f50
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:36:20 +0000

    Feb 7, 2026, 10:06 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -41,8 +41,8 @@ export default function Movie(props) {
                             >
                                 <button
                                     onClick={() => {
-                                        setMovies(movie.id)
-                                        setselectedglobal(movie.id)
+                                        setMovies(movie.)
+                                        setselectedglobal(movie)
                                     }}
                                     className={`bg-black/70 hover:bg-black/70 px-4 py-2 rounded text-white text-sm flex items-center gap-2 ${movies ? "pointer-events-none" : ""}`}
                                 >
