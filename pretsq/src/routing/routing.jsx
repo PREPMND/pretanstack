cefed24d135da76ff2c956266a6e29fb6b2e4923
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:23:57 +0000

    Feb 7, 2026, 8:53 PM

--- pretsq/src/routing/routing.jsx
+++ pretsq/src/routing/routing.jsx
@@ -44,7 +44,7 @@ export default function NavRoutes(props){
                 
                 
                 </>}/>
-                <Route path="*" elem
+                <Route path="*" element
             </Routes>
 
         </>
