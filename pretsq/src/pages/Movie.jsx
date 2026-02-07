50f017310d5cd143060b63e53f05d8e8e32b380d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:53:11 +0000

    Feb 7, 2026, 9:23 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -10,7 +10,7 @@ export default function Movie(){
     return(
         
         <>
-        <div className="bg-neutral-900">
+        <div className="bg-neutral-900 w-full ">
             <div className="bg-neutral-950 grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 pt-2 ">
                 {data?.slice(0,50).map((movie)=>{
                     return(
