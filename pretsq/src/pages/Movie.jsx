c04e106fa29ce6ae6d030f1b6db993023bbfc237
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:28:25 +0000

    Feb 7, 2026, 9:58 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -24,12 +24,12 @@ export default function Movie(props) {
                         return (
                             <div className=" aspect-[2/3] rounded overflow-hidden relative"
                                 key={movie.id}>
-                                <div
-                                    
-                                >   <div onMouseEnter={() => 
+                                onMouseEnter={() => 
                                     setMovies(movie.id)}
                                     onMouseLeave={() => 
-                                    setMovies(null)}>
+                                    setMovies(null)}
+                                    
+                                >   <div >
                                     <img 
                                     
                                     className="rounded object-cover z-50"
