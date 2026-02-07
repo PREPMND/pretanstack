be848bfda4103874f2ec2c0a6602801eabc8b5f7
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 15:46:03 +0000

    Feb 7, 2026, 9:16 PM

--- pretsq/src/api/movies.jsx
+++ pretsq/src/api/movies.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_TMBD_KEY;
 
-export const fetchTopRated = async (page = 1, sort = "popularity.desc") => {
+export const fetchMovies = async (page = 1, sort = "popularity.desc") => {
   const res = await axios.get(
     `https://api.themoviedb.org/3/discover/movie`,
     {
