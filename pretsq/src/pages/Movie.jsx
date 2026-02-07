1780172e4b9e4f5e47743371e2890aea80a39e93
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-07 16:21:51 +0000

    Feb 7, 2026, 9:51 PM

--- pretsq/src/pages/Movie.jsx
+++ pretsq/src/pages/Movie.jsx
@@ -34,7 +34,7 @@ export default function Movie(props) {
                                         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                                     <h2>{movie.title}</h2>
                                     <div
-                                className={`absolute inset-0 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movies === movie ? "opacity-100" : "opacity-0"}`}
+                                className={`absolute inset-0 z-40 flex items-center justify-center bg-black/50 transition-opacity duration-200 ${movies === movie ? "opacity-100" : "opacity-0"}`}
                             >
                                 <button
                                     onClick={() => {
