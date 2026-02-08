00cfa52cff25887f89d441a7b36cce07425f6a0d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:13:02 +0000

    Feb 8, 2026, 4:43 PM

--- pretsq/src/pages/toprated.jsx
+++ pretsq/src/pages/toprated.jsx
@@ -1,4 +1,11 @@
 export default function TOPrated(){
+    const { search , setsearch}=props;
+        const{ data , isLoading , error}=useQuery({
+            queryKey:["search",search],
+            queryFn:()=>fetchSearch(search),
+            enabled:search.length>0,
+        })
+        const sortedData = data ? [...data].sort((a, b) => b.popularity - a.popularity) : [];
     return(
         
         <>
