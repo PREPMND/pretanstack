810b3bca54cdd36046cfebf247dce50bdf759ad4
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:27:45 +0000

    Feb 8, 2026, 4:57 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -28,8 +28,8 @@ export default function SubSearch(props){
                 key={movie.id}>
                     <div>
                         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                    </div>
-                    <h2>{movie.title}</h2>
+                    
+                    <h2>{movie.title}</h2></div>
                 </div>)
 
             })}
