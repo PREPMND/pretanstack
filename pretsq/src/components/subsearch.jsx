7757e286f06dd503fc9cb75b085721a17333ca60
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 10:57:11 +0000

    Feb 8, 2026, 4:27 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -24,9 +24,9 @@ export default function SubSearch(props){
             {sortedData?.slice(0,4).map((movie)=>{
                 <div key={movie.id}>
                     <div>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} /></div>
-                    
-                    <h2>{movie.title}</h2>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                        </div>
+                        <h2>{movie.title}</h2>
                 </div>
 
             })}
