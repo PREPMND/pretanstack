acdcec96d1cb1de663ac5858ce42dbfa11d28107
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:49:47 +0000

    Feb 8, 2026, 5:19 PM

--- pretsq/src/components/navbar.jsx
+++ pretsq/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-
+import { SubSearch } from './subsearch.jsx';
 export default function Navbar(props){
     let navigate=useNavigate();
     const { selected , setselected}=props;
