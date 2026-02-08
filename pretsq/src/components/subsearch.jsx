482065e571b1afecc803520e9bf919ba3b71a459
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:40:32 +0000

    Feb 8, 2026, 5:10 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -20,7 +20,7 @@ export default function SubSearch(props){
             value={search}
             onChange={(e)=>{setsearch(e.target.value)}}
             />
-            <span onClick={()=>{search}}>*</span>
+            <span onClick={()=>{setsearch("")}}>*</span>
         </div></div> 
         {data && (<div className={` w-[400px] z-50 justify-center absolute top-full text-stone-200 font-bold text-lg `}>
             {sortedData?.slice(0,4).map((movie)=>{
