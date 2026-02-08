042cbf0bf17281b88aec592d20c664b87f2191b8
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:49:19 +0000

    Feb 8, 2026, 5:19 PM

--- pretsq/src/routing/layout.jsx
+++ pretsq/src/routing/layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar.jsx";
 import NavRoutes from "./routing.jsx";
+import { useState } from "react";
 export default function Layout(props) {
     const { selected , setselected}=props;
     const {selectedtoprated, setselectedtoprated} = props
