058e00c731be4da5a0a0ce24ba2c91c2b174f990
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 10:30:01 +0000

    Feb 8, 2026, 4:00 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -2,6 +2,7 @@ import { fetchSearch } from "../api/movies"
 import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
 export default function SubSearch(){
+    
     return(
 
         <>
