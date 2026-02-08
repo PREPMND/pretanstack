85d4d4fa33eb754a55d69740b69b3e70f1290e8a
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 10:27:58 +0000

    Feb 8, 2026, 3:57 PM

--- pretsq/src/api/movies.jsx
+++ pretsq/src/api/movies.jsx
@@ -16,7 +16,7 @@ export const fetchMovies = async (page = 1, sort = "popularity.desc") => {
 
   return res.data.results; 
 };
-export const fetchMovies = async (page = 1, sort = "popularity.desc") => {
+export const fetchSearch = async (page = 1, sort = "popularity.desc") => {
   const res = await axios.get(
     `https://api.themoviedb.org/3/discover/movie`,
     {
