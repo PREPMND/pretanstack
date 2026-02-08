69ed089de2b6be20af55606f92860d216b3abe6d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:28:46 +0000

    Feb 8, 2026, 4:58 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -26,10 +26,10 @@ export default function SubSearch(props){
                 return (
                 <div className="bg-neutral-900 p-2 border-b-2 border-white/10 w-full hover:bg-neutral-800 cursor-pointer gap-3"
                 key={movie.id}>
-                    <div className="flex">
+                    <div className="aspect">
                         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                    
-                    <h2>{movie.title}</h2></div>
+                    </div>
+                    <h2>{movie.title}</h2>
                 </div>)
 
             })}
