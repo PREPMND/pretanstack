17a6727842f6528c8441ec0ae7e5b36f7963f68f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:31:26 +0000

    Feb 8, 2026, 5:01 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -28,7 +28,8 @@ export default function SubSearch(props){
                 key={movie.id}>
                     <div>
                     <div className="aspect-[2/3] w-[50px] rounded overflow-hidden ">
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} /></div>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                    </div>
                         <h2>{movie.title}</h2>
                     </div>
                 </div>)
