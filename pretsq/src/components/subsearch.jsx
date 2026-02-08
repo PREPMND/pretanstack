fefde91e22679c2d776690651b69b77ec335ee7d
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 11:33:01 +0000

    Feb 8, 2026, 5:03 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -27,7 +27,7 @@ export default function SubSearch(props){
                 <div className="bg-neutral-900 p-2 border-b-2 m-auto border-white/10 w-[70%] hover:bg-neutral-800 cursor-pointer gap-3"
                 key={movie.id}>
                     <div className="flex items-center gap-3">
-                    <div className="aspect-[2/3] w-[50px] rounded overflow-hidden ">
+                    <div className="aspect-[2/3] w-[50px] rounded object-cover overflow-hidden ">
                         <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                     </div>
                         <h2 className="truncate">{movie.title}</h2>
