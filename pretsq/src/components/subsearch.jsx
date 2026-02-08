4a1df5d52d0ea49c696c6a6416dc8f91fccc228f
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 10:39:44 +0000

    Feb 8, 2026, 4:09 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -14,7 +14,7 @@ export default function SubSearch(props){
         <div className={` min-w-full`} >
         <div className='bg-neutral-900 pt-2 md:hidden flex w-full'>
             <input type="text" id="search" placeholder=' Search...' className='p-1 py-[4px] bg-neutral-700  ml-12 mx-5 rounded-md text-white'
-            
+            value={search}
             />
         </div></div>
         <div>
