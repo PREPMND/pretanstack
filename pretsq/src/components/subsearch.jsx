cb5167f749314279e0c01d450c3d37a1f8bb35b2
PREPMND <219679461+PREPMND@users.noreply.github.com>
2026-02-08 10:39:34 +0000

    Feb 8, 2026, 4:09 PM

--- pretsq/src/components/subsearch.jsx
+++ pretsq/src/components/subsearch.jsx
@@ -13,8 +13,9 @@ export default function SubSearch(props){
         <>
         <div className={` min-w-full`} >
         <div className='bg-neutral-900 pt-2 md:hidden flex w-full'>
-            <input type="text" id="search" placeholder=' Search...' className='p-1 py-[4px] bg-neutral-700  ml-12 mx-5 rounded-md text-white
-                 '/>
+            <input type="text" id="search" placeholder=' Search...' className='p-1 py-[4px] bg-neutral-700  ml-12 mx-5 rounded-md text-white'
+            
+            />
         </div></div>
         <div>
             {data?.slice(0,4).map((movie)=>{
