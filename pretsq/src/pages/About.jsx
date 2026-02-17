e4403acf26f8fe8cd6884e874d63f298b34b211b
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-17 17:40:07 +0530

    refactor About component layout and add hover effect for PrepWatch

--- pretsq/src/pages/About.jsx
+++ pretsq/src/pages/About.jsx
@@ -1,14 +1,25 @@
 import { GitGraph } from "lucide-react";
-
+import { useState } from "react";
 export default function About() {
+    const [select, setselect] =useState(false);
   return (
     <>
-      <div className="h-screen bg-neutral-900 w-full text-rose-50 text-xl font-[Segoe-UI-Emoji] md:text-3xl">
-        <section className="flex justify-between pt-4 px-2 h-full w-full">
-          <div class>PrepWatch</div>
-          <div>About Us</div>
+      <div className="h-screen overflow-hidden bg-neutral-900 w-full text-rose-50 text-xl font-[Segoe-UI-Emoji] md:text-3xl">
+        <section className="flex justify-between pt-4 md:px-5 px-4 h-full w-full">
+        <div>
+          <div onMouseEnter={()=>setselect(true)}
+               onMouseLeave={()=>setselect(false)}
+          className="transition-transform duration-100 ease-in-out hover:translate-x-3">PrepWatch</div>
+          <div className={`${select==true?"opacity-100":"opacity-0"} absolute  w-[70%] h-[45%] md:w-[40%] bg-rose-50 m-3`}>
+
+          </div>
+        </div>
+        <div>About Us</div>
           
         </section>
+        <div className="text-3xl text-white">
+            BiGbIG
+        </div>
       </div>
     </>
   );
