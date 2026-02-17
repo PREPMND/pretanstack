8de723a9ed24c2f399b36f8c5a765904ed96b037
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-17 17:17:14 +0530

    about changes

--- pretsq/src/pages/About.jsx
+++ pretsq/src/pages/About.jsx
@@ -1,18 +1,17 @@
-export default function About(){
-    return(
-        
-        <>
-        <div>
-            <section> 
-                <div>
-                    PrepWatch
-                </div>
-                <div>About Us</div> 
-                <div>
-                    <Git
-                </div>
-            </section>
-        </div>
-        </>
-    )
+import { GitGraph } from "lucide-react";
+
+export default function About() {
+  return (
+    <>
+      <div className="h-full bg-neutral-900 w-full text-white text-3xl">
+        <section className="flex justify-between w-full">
+          <div>PrepWatch</div>
+          <div>About Us</div>
+          <div>
+            
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
