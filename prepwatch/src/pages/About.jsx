ba5cbcaf9902c8b2006fa7a59be68e8e647c3f39
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-17 19:26:12 +0530

    did about

--- prepwatch/src/pages/About.jsx
+++ prepwatch/src/pages/About.jsx
@@ -11,8 +11,8 @@ export default function About() {
           <div onMouseEnter={()=>setselect(true)}
                onMouseLeave={()=>setselect(false)}
           className="transition-transform duration-100 ease-in-out hover:translate-x-3">PrepWatch</div>
-          <div className={`${select==true?"opacity-100":"opacity-0"} absolute  w-[70%] min-h-[45%] md:w-[40%] bg-rose-50 m-3 text-black rounded-lg p-4 text-sm md:text-base transition-opacity duration-300 ease-in-out`}>
-           A production level movie web app built using React, Tailwind CSS, and The Movie Database (TMDb) API. It offers a visually appealing experience for movie enthusiasts to explore a vast collection of films, discover trending titles, and manage their favorites. With a user-friendly interface and responsive design, PrepWatch provides an immersive platform for users to stay updated on the latest releases and find their next cinematic adventure. The app's integration with TMDb ensures access to comprehensive movie data, making it a go-to destination for film lovers seeking an engaging and informative movie browsing experience.
+          <div className={`${select==true?"opacity-100":"opacity-0"} absolute  w-[70%] min-h-fit md:w-[40%] bg-rose-50 m-3 text-black rounded-lg p-4 text-sm md:text-base transition-opacity duration-300 ease-in-out`}>
+           A production level movie web app built using React, Tailwind CSS, and The Movie Database (TMDb) API. It offers a visually appealing experience for movie enthusiasts to explore a vast collection of films, discover trending titles, and manage their favorites.<span className={`md:flex hidden`}> With a user-friendly interface and responsive design, PrepWatch provides an immersive platform for users to stay updated on the latest releases and find their next cinematic adventure. The app's integration with TMDb ensures access to comprehensive movie data, making it a go-to destination for film lovers seeking an engaging and informative movie browsing experience.</span>
            <p>
             React Query /@TanStack has been used for way more efficient data fetching than traditional react. ReactRouter is used for versatile and dynamic routing.
            </p>
@@ -22,11 +22,11 @@ export default function About() {
              onMouseLeave={()=>setselectabout(false)}>
                 
           <span className="hover:text-rose-200">About Us</span>
-        <div className={`${selectabout==true?"opacity-100":"opacity-0"} items-center absolute right-0 w-[70%] min-h-[45%] md:w-[40%] bg-rose-50 m-3 text-black rounded-lg p-4 text-sm md:text-base transition-opacity duration-300 ease-in-out   `}><div className="flex items-center ">
-           <a href="https://github.com/PREPMND/pretanstack" className="text-3xl">GitHub Repository <span className="text-[16px] text-stone-600 ">Click to open</span></a>
-           <CatIcon className="animate-pulse text-rose-500 ml-4" fill="black" size={50}/></div>
+        <div className={`${selectabout==true?"opacity-100":"opacity-0"} items-center absolute right-0 w-[70%] min-h-fit md:w-[40%] bg-rose-50 m-3 text-black rounded-lg p-4 text-sm md:text-base transition-opacity duration-300 ease-in-out   `}><div className="flex items-center ">
+           <a href="https://github.com/PREPMND/pretanstack" className="text-[14px] sm:text-xl md:text-3xl whitespace-wrap w-full">GitHub <span className="sm:flex hidden">Repository</span> <span className="text-[16px] lg:flex hidden text-stone-600 ">Click to open</span></a>
+           <CatIcon className="animate-pulse text-rose-500 ml-4 absolute right-4" fill="black" size={40} /></div>
            <div>
-            <img src="https://preview.redd.it/progetto-meme-del-mascellone-che-mette-la-mano-sulla-spalla-v0-rceycnd8y99f1.png?width=4880&format=png&auto=webp&s=cc3ab8d0e02861fbd11ae773a3a6a110b63884f2" alt="meme" className="rounded mt-4"/>
+            <img src="https://preview.redd.it/progetto-meme-del-mascellone-che-mette-la-mano-sulla-spalla-v0-rceycnd8y99f1.png?width=4880&format=png&auto=webp&s=cc3ab8d0e02861fbd11ae773a3a6a110b63884f2" alt="meme" className="rounded-lg mt-4"/>
            </div>
           </div>
         </div>
