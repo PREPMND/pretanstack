6c902cacb676a84215dcc78474cf5870afd9bec0
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-19 12:43:58 +0530

    Refactor Favourites component layout and styling for improved user experience

--- prepwatch/src/components/subsearch.jsx
+++ prepwatch/src/components/subsearch.jsx
@@ -62,10 +62,10 @@ export default function SubSearch(props) {
                          className={`text-amber-300 ml-5 ${rotate || !status ? "animate-spin" : "animate-none"}`} size={18} 
                          
                          />
-                        <div className={`${status==="Offline"?"block":"hidden"} text-rose-600 text-[8px]  md:text-sm right-24 absolute md:flex hidden`}><div className="">Looks like you're </div>Offline</div>
+                        <div className={`${status==="Offline"?"block":"hidden"} text-rose-600 text-[8px]  md:text-sm right-24 absolute `}><div className="md:flex hidden">Looks like you're Offline </div></div>
                         <div className="absolute left-2 md:right-5 md:left-auto">
                             {status === "Offline" && (<WifiOff className="m-auto animate-pulse text-white mb-1" size={25} />)}
-                            {status === "Online" && (<WifiHigh className="m-auto text-green-500 mb-2 md:mb-1" size={30} />)}
+                            {status === "Online" && (<WifiHigh className="m-auto text-green-500  md:mb-1" size={30} />)}
                         </div>
                     </div>
 
--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -3,7 +3,10 @@ export default function Favourites(){
         
         <>
         <div>
-            <div>Favourites</div>
+            <div className="h-[calc(100vh-120px)] bg-neutral-900 ">
+                <div className="text-white text-center pt-32 text-2xl font-semibold">Your favourite movies will appear here !</div>
+                
+            </div>
         </div>
         </>
     )
