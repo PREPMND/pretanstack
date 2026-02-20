e8b5bd3aaf69a9ba8973734cab181f13fdc0ac22
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 12:58:01 +0000

    Feb 20, 2026, 6:28 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -14,6 +14,9 @@ export default function Favourites(props){
                             <div key={movie.id} className="flex items-center justify-between w-[90%] mx-auto mt-5 p-3 bg-neutral-800 rounded-lg">
                                 <div className="flex items-center">
                                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-16 h-24 rounded-md mr-4" />
+                                    <div>
+                                        <h2 className="text-lg font-semibold">{movie.title}</h2>
+                                        <p className="text-sm text-gray-400">{movie.release_date}</p>   
                         )
                     })}</div>
                 </div>
