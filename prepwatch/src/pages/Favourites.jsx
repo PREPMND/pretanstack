a9255e6404f0e63fbee4717f6f18cbb87d5947c7
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 12:56:51 +0000

    Feb 20, 2026, 6:26 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -1,13 +1,14 @@
 
 export default function Favourites(props){
     const {favourites , setFavourites} = props;
+    const {heart, setheart} = props;
     return(
         
         <>
         <div>
             <div className="h-[calc(100vh-120px)] bg-neutral-900 ">
                 <div className="text-white text-center pt-32 text-2xl font-semibold">Your favourite movies will appear here !</div>
-                <div
+                <div 
             </div>
         </div>
         </>
