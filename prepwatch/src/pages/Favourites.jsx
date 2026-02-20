bc833ccde3bd18811e23a2f6a73f58c9c9484631
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 12:56:59 +0000

    Feb 20, 2026, 6:26 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -8,7 +8,7 @@ export default function Favourites(props){
         <div>
             <div className="h-[calc(100vh-120px)] bg-neutral-900 ">
                 <div className="text-white text-center pt-32 text-2xl font-semibold">Your favourite movies will appear here !</div>
-                <div 
+                
             </div>
         </div>
         </>
