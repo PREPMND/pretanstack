2caf4e2330d370194afee816a558b6d0f4c28610
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 13:04:11 +0000

    Feb 20, 2026, 6:34 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -9,7 +9,7 @@ export default function Favourites(props){
             <div className="h-[calc(100vh-120px)] bg-neutral-900 ">
                 <div className="text-white text-center pt-32 text-2xl font-semibold">Your favourite movies will appear here !</div>
                 <div>
-                    <div class
+                    <div className=""
                     >{localStorage.getItem("favourites") && JSON.parse(localStorage.getItem("favourites"))?.map((movie)=>{
                         return(
                             <div key={movie.id} className="flex items-center justify-between w-[90%] mx-auto mt-5 p-3 bg-neutral-800 rounded-lg">
