90a74f416690dba1e1e1dbbf9fc7ea7fc5b9d5eb
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 13:13:04 +0000

    Feb 20, 2026, 6:43 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -13,7 +13,8 @@ export default function Favourites(props){
                     >{localStorage.getItem("favourites") && JSON.parse(localStorage.getItem("favourites"))?.map((movie)=>{
                         return(
                             <div key={movie.id} className="flex items-center justify-between w-[90%] mx-auto mt-5 p-3 bg-neutral-800 rounded-lg">
-                                <div className="flex w-full items-center">
+                                <div 
+                                className="flex w-full items-center">
                                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-16 h-24 rounded-md mr-4" />
                                     <div className="">
                                         <h2 className="text-lg text-white transition-colors duration-300 ease-in-out hover:text-stone-400  font-semibold">{movie.title}</h2>
