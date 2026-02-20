ec9d5a0e3a491984ba90c5db37c2efd5b56e3555
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-20 13:07:00 +0000

    Feb 20, 2026, 6:37 PM

--- prepwatch/src/pages/Favourites.jsx
+++ prepwatch/src/pages/Favourites.jsx
@@ -19,7 +19,7 @@ export default function Favourites(props){
                                         <h2 className="text-lg text-white  font-semibold">{movie.title}</h2>
                                         <p className="text-sm text-gray-400">{movie.release_date}</p>   
                                     </div>
-                                    <div className=></div>
+                                    <div className={`${adult}`}></div>
                                 </div>
                             </div>
                         )
