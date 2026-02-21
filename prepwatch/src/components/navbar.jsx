22dcde053fbf7211541cdfd399bf9c31ad38bb21
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 06:32:54 +0000

    Feb 21, 2026, 12:02 PM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar(props){
                     onClick={()=>{
                         setbackdrop(true);
                     }}
-                    className={`md:hidden ab flex`}/>
+                    className={`md:hidden ml-2 flex`}/>
                 </div>
                 <div>
                     <button onClick={()=>navigate('/about')} className='bg-emerald-800 active:scale-95 active:bg-emerald-500 px-2 py-1 rounded-md ml-4 md:flex hidden active:text-slate-50'>About</button>
