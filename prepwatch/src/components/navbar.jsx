c0961ce4c3d304eec80e8b5b89b338c0f57ea973
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 05:47:12 +0000

    Feb 21, 2026, 11:17 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -24,7 +24,9 @@ export default function Navbar(props){
                 </div>
                 <div>
                     <List
-                    
+                    onClick={()=>{
+                        set
+                    }}
                     className={`md:hidden flex`}/>
                 </div>
                 <div>
