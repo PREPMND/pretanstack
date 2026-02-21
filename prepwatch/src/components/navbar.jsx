09191fd3a74357d72997b0b28622cf0f275af7cc
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 05:51:37 +0000

    Feb 21, 2026, 11:21 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar(props){
             </div>
         </div>
         {backdrop && <div 
-        
+        onC
         className='absolute top-0 right-0  h-screen bg-neutral-900 z-50 flex flex-col items-center justify-center w-[60%] md:w-[40%] gap-4'>
             
         </div>}
