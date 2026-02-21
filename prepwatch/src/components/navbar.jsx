d8ce55955da60d9dd2847d949c9a9279b5106572
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 06:05:12 +0000

    Feb 21, 2026, 11:35 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -45,7 +45,7 @@ export default function Navbar(props){
     {/* Side Menu */}
     <div
       onClick={(e) => e.stopPropagation()}
-      className="absolute top-0 right-0 h-screen bg-neutral-900 w-[60%] md:w-[40%] flex flex-col   gap-4 items-start *:ml-3 text-rose-50 "
+      className="absolute top-0 right-0 h-screen bg-neutral-900 w-[60%] md:w-[40%] flex flex-col   gap-4 items-start *:ml-3 mt-5 text-rose-50 "
     >
       <button onClick={() => { navigate('/movie'); setbackdrop(false); }}>
         Movies
