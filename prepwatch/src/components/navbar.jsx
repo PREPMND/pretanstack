19b88721a694047cbe960d38912663eb6f14ef62
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 06:21:09 +0000

    Feb 21, 2026, 11:51 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -47,14 +47,14 @@ export default function Navbar(props){
       onClick={(e) => e.stopPropagation()}
       className="absolute top-0 ml-83  right-[0] h-screen bg-neutral-700 w-[60%] md:w-[40%] flex flex-col   gap-4 items-start *:ml-3 pt-5 text-rose-50 rounded-l-3xl p-5 "
     >
-      <div className="text-xl text-gray-400 pb-4 font-bold">MenuBar</div>
+      <div className="text-xl text-gray-400 pb-4 w-full font-bold">MenuBar</div>
       <button className='hover:text-rose-100 flex hover:bg-neutral-800 '
       onClick={() => { navigate('/movie'); setbackdrop(false); }}>
         <Popcorn className="mr-2 "/> 
         Movies
       </button>
 
-      <button className='hover:text-rose-100 hover:bg-neutral-800 flex'
+      <button className='hover:text-rose-100 w-full hover:bg-neutral-800 flex'
       onClick={() => { navigate('/favourites'); setbackdrop(false); }}>
         <BookHeart className="mr-2"/>
         Favourites
