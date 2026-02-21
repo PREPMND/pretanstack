f57b4af3a71d8e979cdedc632253e414efb9e263
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 05:58:10 +0000

    Feb 21, 2026, 11:28 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -8,10 +8,7 @@ export default function Navbar(props){
     return(
         <>
         <div 
-        onClick={(e)=>{
-            setbackdrop(false)
-            e.stopPropagation()
-        }}
+        
         className='flex *:flex h-[64px] bg-amber-400 w-full p-4 justify-between *:justify-between md:*:gap-4 *:gap-2
         items-center *:items-center text-white font-semibold'>
             <div>
