16bad3aa1a9af6affcafab2812acf7b48f39281f
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-21 06:27:13 +0000

    Feb 21, 2026, 11:57 AM

--- prepwatch/src/components/navbar.jsx
+++ prepwatch/src/components/navbar.jsx
@@ -54,13 +54,13 @@ export default function Navbar(props){
         Movies
       </button>
 
-      <button className='hover:text-rose-100 px-2 pl-2 w-full hover:bg-neutral-800 flex'
+      <button className='hover:text-rose-100 py-2 pl-2 w-full hover:bg-neutral-800 flex'
       onClick={() => { navigate('/favourites'); setbackdrop(false); }}>
         <BookHeart className="mr-2"/>
         Favourites
       </button>
 
-      <button className='hover:text-rose-100 px-2 pl-2 w-full hover:bg-neutral-800 flex'
+      <button className='hover:text-rose-100 py-2 pl-2 w-full hover:bg-neutral-800 flex'
       onClick={() => { navigate('/about'); setbackdrop(false); }}>
         <Box className="mr-2"/>
         About
