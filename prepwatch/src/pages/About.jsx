c6a9c5054b6fe491c3e1a046b2b3825753c5704c
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-22 09:28:19 +0000

    Feb 22, 2026, 2:58 PM

--- prepwatch/src/pages/About.jsx
+++ prepwatch/src/pages/About.jsx
@@ -41,7 +41,7 @@ export default function About() {
         onMouseLeave={()=>setselectinfo(false)}
         className="absolute left-1/2 top-1/2 transform -translate-x-1/2 transition-transform hover:-translate-x-1 duration-[1200ms] ease-in-out ">Hover Over Them , Info's There</div>
         <div>
-          <img src="https://static.boredpanda.com/blog/wp-content/uploads/2025/10/funny-cat-memes-go-hard-cover_675.jpg" alt="cat meme" className={`${sele} absolute bottom-0 left-0 w-[200px] md:w-[300px] rounded-lg m-4 animate-pulse`} />
+          <img src="https://static.boredpanda.com/blog/wp-content/uploads/2025/10/funny-cat-memes-go-hard-cover_675.jpg" alt="cat meme" className={`${selectinfo?"opacity-100":"opacity-0"} absolute bottom-0 left-0 w-[200px] md:w-[300px] rounded-lg m-4 animate-pulse`} />
         </div>
         <div className="absolute bottom-0 w-full text-center p-4 text-sm text-stone-500">
           
