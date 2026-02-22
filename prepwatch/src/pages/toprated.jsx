c974f7f78f1e8ce60c4a447b56c44d46c1451ae9
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-22 09:35:52 +0000

    Feb 22, 2026, 3:05 PM

--- prepwatch/src/pages/toprated.jsx
+++ prepwatch/src/pages/toprated.jsx
@@ -15,8 +15,8 @@ export default function Topratedpage(props) {
         refetchOnMount: true,
         refetchOnWindowFocus:true,
     })
-    
-    if (isLoading) return(<div className={`w-full min-h-[calc(100vh-120px)] h-full bg-neutral-900 z-40 flex items-center justify-center whitespace-nowrap text-yellow-50 font-[Inter] font-[600] `}>
+    const setl = true
+    if (setl) return(<div className={`w-full min-h-[calc(100vh-120px)] h-full bg-neutral-900 z-40 flex items-center justify-center whitespace-nowrap text-yellow-50 font-[Inter] font-[600] `}>
           <LoaderPinwheel className="animate-spin mr-3"/>
           <div>Loading...</div>
           <div className='absolute bottom-4 text-center font-sans text-stone-400 md:text-[14px]text-[8px]'>Results are on their way ! <span className="md:flex hidden ">Check your network connection.</span></div>
