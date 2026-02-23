e1b7e016df09502e6a8fd1011726f7d29372192b
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-23 14:58:38 +0000

    Feb 23, 2026, 8:28 PM

--- prepwatch/src/indvcomponents/indvpopular.jsx
+++ prepwatch/src/indvcomponents/indvpopular.jsx
@@ -15,7 +15,7 @@ export default function IndvPopular(props) {
         queryKey: ["credits", id],
         queryFn: () => fetchMovieCredits(id),
     })
-    const trailer = videos?.results?.find((vid) => vid.type === "Trailer" || vid.type === "Teaser" && vid.site === "YouTube");
+    const trailer = videos?.results?.find((vid) => (vid.type === "Trailer" || vid.type === "Teaser") && vid.site === "YouTube");
     console.log(selected)
     return (
         <>
