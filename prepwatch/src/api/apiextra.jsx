7ccf123a1dc043d6edede3be866fdf34a77fac41
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-23 14:51:08 +0000

    Feb 23, 2026, 8:21 PM

---
+++ prepwatch/src/api/apiextra.jsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_TMDB_KEY;
+
+export const fetchMovieCredits = async (id) => {
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}/credits`,
+    { params: { api_key: API_KEY } }
+  );
+  return res.data;
+};
+
+export const fetchMovieVideos = async (id) => {
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}/videos`,
+    { params: { api_key: API_KEY } }
+  );
+  return res.data;
+};
