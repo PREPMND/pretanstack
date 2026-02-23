daf0fcd3887e2a34a8ac06fbfc87062d3cd2f428
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-23 14:53:18 +0000

    Feb 23, 2026, 8:23 PM

--- prepwatch/src/indvcomponents/indvpopular.jsx
+++ prepwatch/src/indvcomponents/indvpopular.jsx
@@ -7,11 +7,11 @@ import { fetchMovieVideos } from "../api/apiextra";
 export default function IndvPopular(props){
     const { id }=useParams();
     const { selected , setselected}=props;
-    const { data:, isLoading, error } = useQuery({
+    const { data:credits, isLoading, error } = useQuery({
         queryKey: ["teaser", id],
         queryFn: () => fetchMovieVideos(id),
     })
-    const 
+    const {}
     console.log(selected)
     return(
         <>
