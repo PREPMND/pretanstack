3ae371c67a3a6c33dc3b9eea99f51c3ef218d1d1
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-23 14:51:20 +0000

    Feb 23, 2026, 8:21 PM

--- prepwatch/src/api/apiextra.jsx
+++ prepwatch/src/api/apiextra.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
-
 const API_KEY = import.meta.env.VITE_TMDB_KEY;
-
 export const fetchMovieCredits = async (id) => {
   const res = await axios.get(
     `https://api.themoviedb.org/3/movie/${id}/credits`,
