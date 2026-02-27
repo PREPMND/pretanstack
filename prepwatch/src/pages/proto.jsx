fa49d3c8ef256297ca1f806cee2afbb6694940e8
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:47:01 +0000

    Feb 27, 2026, 7:17 PM

--- prepwatch/src/pages/proto.jsx
+++ prepwatch/src/pages/proto.jsx
@@ -1,5 +1,5 @@
 import React, { use } from 'react'
-import { GridLayout } from '../indvcomponents/gridlayout'
+import { GridLayout } from '../indvcomponents/functions'
 import { useQuery ,useNavigate } from "@tanstack/react-query"
 import { fetchPopularMovies } from '../api/apiextra'
 import { Heart } from 'lucide-react'
