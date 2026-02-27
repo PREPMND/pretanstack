121bec2d9f3a92d734807863b58bdc2d0db4e215
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:48:58 +0000

    Feb 27, 2026, 7:18 PM

--- prepwatch/src/indvcomponents/functions.jsx
+++ prepwatch/src/indvcomponents/functions.jsx
@@ -5,7 +5,7 @@ export function GridLayout(navigate, data, setselected, selected ,movieHovered, 
         <div className="pt-1">
           <button onClick={() => navigate("/movie")}
             className="px-3 font-Inter tracking-wide text-[13px] md:text-[14px]">Popular Movies</button>
-          <button onClick={() => setnum(prev => prev + 1)}
+          <button
             className="ml-3 mt-2 m-auto md:text-[15px] font-[600] text-[13px] bg-white/20 hover:bg-white/30 text-white backdrop-blur-sm px-2 py-1 rounded active:scale-95">More Popular Movies</button>
         </div>
         <div className={`grid grid-flow-col select-none auto-cols-[140px] md:auto-cols-[190px] gap-2 md:gap-4 overflow-y-hidden mt-3 scroll-smooth scrollbar-hide overflow-x-auto focus:ring-2 focus:ring-amber-400 px-3 pt-2 ${selected ? "pointer-events-none" : ""}`}>
