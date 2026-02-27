84371b8aca8db3123afd5ed880a53d34007b2a33
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:58:37 +0000

    Feb 27, 2026, 7:28 PM

--- prepwatch/src/pages/proto.jsx
+++ prepwatch/src/pages/proto.jsx
@@ -1,6 +1,6 @@
 
 import {GridLayout } from '../indvcomponents/functions'
-import { useQuery,useNavigate} from "@tanstack/react-query"
+import { useQuery,useNavigate} from "react-router-dom"
 import { fetchPopularMovies } from '../api/apiextra'
 
 import { useState } from 'react'
