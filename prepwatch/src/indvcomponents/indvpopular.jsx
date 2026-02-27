0d488dbce8fbda7488c0a845f10992a1c0092bb9
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 10:53:09 +0000

    Feb 27, 2026, 4:23 PM

--- prepwatch/src/indvcomponents/indvpopular.jsx
+++ prepwatch/src/indvcomponents/indvpopular.jsx
@@ -3,7 +3,7 @@ import { fetchPopularMovies } from "../api/popularapi"
 import { useParams } from "react-router-dom";
 import { ChangeTitle } from "../App";
 import { Popcorn } from "lucide-react"
-import { fetchMovieCredits, fetchMovieVideos } from "../api/apiextra";
+import { fetchMovieCredits, fetchMovieVideos } from "../api/api";
 export default function IndvPopular(props) {
     const { id } = useParams();
     const { selected, setselected } = props;
