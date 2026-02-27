2abda001d919ea0c074c94b150fb2c207a4634b1
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 14:13:24 +0000

    Feb 27, 2026, 7:43 PM

--- prepwatch/src/indvcomponents/functions.jsx
+++ prepwatch/src/indvcomponents/functions.jsx
@@ -1,7 +1,7 @@
 import { Heart } from "lucide-react"
 
 export function GridLayout(navigate, data, x, selected ,movieHovered, setmovieHovered, favourites, setfavourites, heart, setheart){
-    
+    const 
     return(
         <>
         <div className="bg-neutral-900  font-Inter text-slate-50">
