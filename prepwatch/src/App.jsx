7f99e8b99f2a18bb497d4614b67ebeb93643d0c9
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:48:22 +0000

    Feb 27, 2026, 7:18 PM

--- prepwatch/src/App.jsx
+++ prepwatch/src/App.jsx
@@ -1,9 +1,8 @@
 import Navbar from "./components/navbar"
 import NavRoutes from "./routing/routing"
-import { BrowserRouter } from "react-router-dom"
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"    
+import { BrowserRouter } from "react-router-dom"  
 import { useState } from "react"
-import SubSearch from "./components/subsearch"
+
 
 export default function App(){
     const [selectedtoprated,setselectedtoprated]=useState(null)
