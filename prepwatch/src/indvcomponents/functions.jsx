00c3e6db6e41fefe6aa44b879a1576f00f8e11c6
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 14:14:37 +0000

    Feb 27, 2026, 7:44 PM

--- prepwatch/src/indvcomponents/functions.jsx
+++ prepwatch/src/indvcomponents/functions.jsx
@@ -1,6 +1,6 @@
 import { Heart } from "lucide-react"
 import { useState } from "react";
-export function GridLayout(navigate, data, x, selected ,movieHovered, setmovieHovered, favourites, setfavourites, heart, setheart){
+export function GridLayout(navigate, data, x, selected ,favourites, setfavourites, heart, setheart){
     const [movieHovered, setmovieHovered] = useState(null);
     return(
         <>
