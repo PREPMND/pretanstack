cac917cdf6bbf197229c52afe639b3db6e67d6ed
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:38:19 +0000

    Feb 27, 2026, 7:08 PM

--- prepwatch/src/App.jsx
+++ prepwatch/src/App.jsx
@@ -36,7 +36,7 @@ export function ChangeTitle(moviename){
 
 
 }
-export function GridLayout(navigate, data, setselected, movieHovered, setmovieHovered, favourites, setfavourites, heart, setheart){
+export function GridLayout(navigate, data, setselected, selected ,movieHovered, setmovieHovered, favourites, setfavourites, heart, setheart){
     return(
         <>
         <div className="bg-neutral-900  font-Inter text-slate-50">
