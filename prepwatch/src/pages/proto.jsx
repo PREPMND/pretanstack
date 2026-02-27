149cfd90646769e72f6608550e036d3a4ce26470
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:39:03 +0000

    Feb 27, 2026, 7:09 PM

--- prepwatch/src/pages/proto.jsx
+++ prepwatch/src/pages/proto.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { GridLayout } from '../App'
 const Proto = (props) => {
+    const { data, isLoading, error } = useQuery({
+    queryKey: ["popular-movies"],
+    queryFn: () => fetchPopularMovies(),
+    staleTime:0,
+    refetchOnMount:true,
+    refetchOnWindowFocus:true,
+  });
   return (
     <div>
       {GridLayout(props.navigate, props.data, props.setselected, props.movieHovered, props.setmovieHovered, props.setselected, props.selected , props.favourites, props.setfavourites, props.heart, props.setheart)}
