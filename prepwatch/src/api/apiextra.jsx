4309ded617d968f54136c462d33bdf685ba04c1a
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 10:51:31 +0000

    Feb 27, 2026, 4:21 PM

--- prepwatch/src/api/apiextra.jsx
+++ prepwatch/src/api/apiextra.jsx
@@ -15,3 +15,31 @@ export const fetchMovieVideos = async (id) => {
   );
   return res.data;
 };
+xport const fetchMovies = async (page = 1, sort = "popularity.desc") => {
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/discover/movie`,
+    {
+      params: {
+        api_key: API_KEY,
+        page: page,
+        sort_by: sort,
+      },
+    }
+  );
+
+  return res.data.results; 
+};
+export const fetchSearch = async (search) => {
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/search/movie`,
+    {
+      params: {
+        api_key: API_KEY,
+        type:"search",
+        query:search,
+      },
+    }
+  );
+
+  return res.data.results; 
+};
--- prepwatch/src/api/movies.jsx
+++ prepwatch/src/api/movies.jsx
@@ -2,31 +2,4 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_TMBD_KEY;
 
-export const fetchMovies = async (page = 1, sort = "popularity.desc") => {
-  const res = await axios.get(
-    `https://api.themoviedb.org/3/discover/movie`,
-    {
-      params: {
-        api_key: API_KEY,
-        page: page,
-        sort_by: sort,
-      },
-    }
-  );
-
-  return res.data.results; 
-};
-export const fetchSearch = async (search) => {
-  const res = await axios.get(
-    `https://api.themoviedb.org/3/search/movie`,
-    {
-      params: {
-        api_key: API_KEY,
-        type:"search",
-        query:search,
-      },
-    }
-  );
-
-  return res.data.results; 
-};
+e
