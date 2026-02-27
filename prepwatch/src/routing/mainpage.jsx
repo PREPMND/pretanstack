66e9d8a6a994c1d845ad48134ba8c54899b2760e
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 11:13:23 +0000

    Feb 27, 2026, 4:43 PM

--- prepwatch/src/routing/mainpage.jsx
+++ prepwatch/src/routing/mainpage.jsx
@@ -12,7 +12,7 @@ const Mainpage = (props) => {
     const {page ,setpage}=props;
   return (
     <div>
-      {<div>
+      {page=="ant<div>
         <div className={`w-full max-h-[calc(100vh-120px)] bg-neutral-900 z-40 flex items-center justify-center text-yellow-50 font-[Inter] font-[600] ${page!==null?"hidden":"block"}`}>
           <LoaderPinwheel className="animate-spin mr-3"/>
           <div>Loading...</div>
