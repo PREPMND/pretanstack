6f93516bc7ff5d1bb4c00aeb5c2ce7c6f3a48c9f
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 10:53:19 +0000

    Feb 27, 2026, 4:23 PM

--- prepwatch/src/components/popular.jsx
+++ prepwatch/src/components/popular.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchPopularMovies } from "../api/popularapi.jsx";
+import { fetchPopularMovies } from "../api/api.jsx";
 import { useState } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { ChangeTitle } from "../App.jsx";
