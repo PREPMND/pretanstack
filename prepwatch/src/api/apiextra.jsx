fdd25b0618724a6f6d62677b3b6cd534d700a5d4
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 10:52:12 +0000

    Feb 27, 2026, 4:22 PM

--- prepwatch/src/api/apiextra.jsx
+++ prepwatch/src/api/apiextra.jsx
@@ -56,3 +56,16 @@ export const fetchPopularMovies = async (page = 1) => {
 
   return res.data.results; 
 };
+export const fetchTopRated = async (page = 1) => {
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/movie/top_rated`,
+    {
+      params: {
+        api_key: API_KEY,
+        page: page,
+      },
+    }
+  );
+
+  return res.data.results; 
+};
--- prepwatch/src/api/trendingapi.jsx
+++ prepwatch/src/api/trendingapi.jsx
@@ -2,16 +2,3 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_TMBD_KEY;
 
-export const fetchTrending = async (page = 1) => {
-  const res = await axios.get(
-    `https://api.themoviedb.org/3/trending/movie/week`,
-    {
-      params: {
-        api_key: API_KEY,
-        page: page,
-      },
-    }
-  );
-
-  return res.data.results; 
-};
