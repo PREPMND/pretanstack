256929cad6ad01fe048cf4881d35a90daa205b1a
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 10:53:54 +0000

    Feb 27, 2026, 4:23 PM

--- prepwatch/src/components/toprated.jsx
+++ prepwatch/src/components/toprated.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchTopRated } from "../api/topratedapi.jsx";
+import { fetchTopRated } from "../api/api.jsx";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Heart } from "lucide-react";
