bf5c58dec966614ec999955f0a631382ecd48725
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:42:35 +0000

    Feb 27, 2026, 7:12 PM

--- prepwatch/src/pages/proto.jsx
+++ prepwatch/src/pages/proto.jsx
@@ -14,7 +14,7 @@ const Proto = (props) => {
   });
   return (
     <div>
-      {GridLayout(props.navigate, data, props.setselected, props.movieHovered, props.setmovieHovered, props.setselected, props.selected , props.favourites, props.setfavourites, props.heart, props.setheart)}
+      {GridLayout(props.navigate, data, props.setselected,  props.movieHovered, props.setmovieHovered, props.setselected, props.selected , props.favourites, props.setfavourites, props.heart, props.setheart)}
     </div>
   )
 }
