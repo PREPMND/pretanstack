f0fd9838d3aa67d79dcd4faefc4fcca980ff516d
PREPMND <219679461+PREPMND@users.noreply.github.com.>
2026-02-27 13:45:41 +0000

    Feb 27, 2026, 7:15 PM

--- prepwatch/src/pages/proto.jsx
+++ prepwatch/src/pages/proto.jsx
@@ -16,7 +16,7 @@ const Proto = (props) => {
   const {favourites, setfavourites} = props;
   const {heart, setheart} = props;
   const [movieHovered , setmovieHovered]=useState(null);
-  
+  const {selectedglobal, setselectedglobal} = props;
   return (
     <div>
       {GridLayout(props.navigate, data, setselected,  movieHovered, setmovieHovered, setselected, selected , favourites, setfavourites, heart, setheart)}
